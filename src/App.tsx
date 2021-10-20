44ddb65154e7225892254494e296d9f075d89f75
bbroger1 <bb.roger@rcssoft.com.br>
2021-10-19 23:02:42 -0300

    Controle de despesas simples, sem uso de banco de dados

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'; //useEffect monitora as alterações dos dados
+import * as C from './App.styles'; //importando tudo que está sendo exportado de dentro da pasta
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
+import { InputArea } from './components/InputArea';
 
-function App() {
+const App = () => {
+
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+
+  }, [list, currentMonth]);
+
+  useEffect(() => {
+    let incomeSum = 0;
+    let expenseSum = 0;
+
+    for (let i in filteredList) {
+      if (categories[filteredList[i].category].expense) {
+        expenseSum += filteredList[i].value;
+      } else {
+        incomeSum += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeSum);
+    setExpense(expenseSum);
+  }, [filteredList]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  };
+
+  const handleAddItem = (item: Item) => {
+    let newList = [...list];
+    newList.push(item);
+    setList(newList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Controle Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
+        <InputArea onAdd={handleAddItem} />
+        <TableArea list={filteredList} />
+      </C.Body>
+    </C.Container>
   );
-}
+};
 
 export default App;
